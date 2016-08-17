904fd0f83e10beeed7b8c4413a5f1847674e2d73
Olney-James <JamesOlney1@gmail.com>
2016-08-17 13:39:44 -0600

    Added core files to publish project

---
+++ healthscore/healthtips.js
@@ -0,0 +1,46 @@
+exerciseScoreTips([
+    {
+        "num": "0",
+        "tip": "Having a late night craving? Eat fruit! It's much better for you than candy or cake."
+    },
+    {
+        "num": "1",
+        "tip": "Having a hard time getting motivated? Grab a work out buddy!"
+    },
+    {
+        "num": "2",
+        "tip": "Comfortable sneakers are important! If you work out a lot, you don't want to be uncomfortable!"
+    },
+    {
+        "num": "3",
+        "tip": "Take time to create your playlist! A good playlist can be the difference between finishing a run and not."
+    },
+    {
+        "num": "4",
+        "tip": "If you want to weigh yourself, do it early in the morning before drinking or eating."
+    },
+    {
+        "num": "5",
+        "tip": "Having a hard time with portion control? Use a smaller plate or bowl! It'll make you feel like you're eating more."
+    },
+    {
+        "num": "6",
+        "tip": "Had a cheat day? Don't worry about it! It takes 3,500 calories to gain a pound of fat. Spend the next few days chipping away at the extra calories."
+    },
+    {
+        "num": "7",
+        "tip": "Tired of the same old vegetables and fruit? Spice up your diet! Experiment with spices and vegetables."
+    },
+    {
+        "num": "8",
+        "tip": "It's been proven that eating in front of your computer leads to mindless eating. Take time out in your day to sit and eat. It will help you control what you eat."
+    },
+    {
+        "num": "9",
+        "tip": "3 pm is a common snacking time. That's awesome! Choose a 100 calorie granola bar or some fresh fruits and veggies to hold you over until dinner."
+    },
+    {
+        "num": "10",
+        "tip": "After you lose the weight, don't stop there! Keep an eye on the scale for extra pounds. Continue to choose healthy food."
+    }
+]);
